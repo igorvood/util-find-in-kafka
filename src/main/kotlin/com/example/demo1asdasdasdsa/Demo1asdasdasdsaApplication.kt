ff636d349a580c0bd32fd62ace1933d2a8e3a599
vood <balitskY_igor@list.ru>
2023-02-20 15:02:23 +0300

    1

---
+++ src/main/kotlin/com/example/demo1asdasdasdsa/Demo1asdasdasdsaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo1asdasdasdsa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demo1asdasdasdsaApplication
+
+fun main(args: Array<String>) {
+    runApplication<Demo1asdasdasdsaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demo1asdasdasdsa/Demo1asdasdasdsaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demo1asdasdasdsa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Demo1asdasdasdsaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
