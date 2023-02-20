0d7c7a13c391c37dd5efe8d983aadcfe6bb8ab64
vood <balitskY_igor@list.ru>
2023-02-20 15:26:10 +0300

    1

--- src/main/kotlin/ru/vood/kafkatracer/appProps/CacheProperty.kt
+++
@@ -1,8 +0,0 @@
-package ru.vood.kafkatracer.appProps
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
-
-@ConstructorBinding
-@ConfigurationProperties(prefix = "configuration.server.cache")
-data class CacheProperty (val timeSafety: Long)
